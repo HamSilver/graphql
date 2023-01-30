48ed4f2fc623eda6fd18cb6668ed8e317276abde
Stanislav Kolpakov <kolpakoff@gmail.com>
2023-01-30 15:50:57 +0200

    feat: Implement get by id for user,profile,post,memberType in one query

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,14 +1,14 @@
-import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { GraphQLSchema } from 'graphql';
-import { getGraphQLQuery } from './getGraphQLQuery';
-import { graphqlBodySchema } from './schema';
-import { graphql } from 'graphql';
+import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
+import { GraphQLSchema } from "graphql";
+import { getGraphQLQuery } from "./getGraphQLQuery";
+import { graphqlBodySchema } from "./schema";
+import { graphql } from "graphql";
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
   fastify.post(
-    '/',
+    "/",
     {
       schema: {
         body: graphqlBodySchema,
@@ -16,6 +16,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
     },
     async function (request, reply) {
       const source = `${request.body.query}`;
+      const variableValues = request.body.variables;
       const schema = new GraphQLSchema({
         query: await getGraphQLQuery(this),
       });
@@ -23,6 +24,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       return await graphql({
         schema,
         source,
+        variableValues,
       });
     }
   );
